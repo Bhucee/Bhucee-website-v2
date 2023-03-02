4dd0c4a460e3c9aa81664aac5774347d95a8200d
Bhucee <125989269+Bhucee@users.noreply.github.com>
2023-03-02 13:17:36 +0000

    added dynamic functions

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Lagos',
+  'Salary': 'N 6,500,000'
+}, {
+  'id': 2,
+  'title': 'Business Analyst',
+  'location': 'Remote',
+  'Salary': 'N 8,500,000'
+}, {
+  'id': 3,
+  'title': 'Back-end Engineer',
+  'location': 'Abuja',
+  'Salary': 'N 7,500,000'
+}, {
+  'id': 4,
+  'title': 'Front-end Engineer',
+  'location': 'Ikeja, LAgos',
+  'Salary': 'N 7,500,000'
+}]
+
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+  return render_template('home.html', jobs=JOBS, company_name='Bhucee Limited')
 
 
 if __name__ == "__main__":
